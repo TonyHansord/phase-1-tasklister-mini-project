43b95456f0406cc04a7cc3f93909b0d0d42d4a1a
Anthony Hansord <tonyhansorddev@gmail.com>
2022-11-24 17:51:10 +1100

    Completed Basic Function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  const taskDescription = document.getElementById("new_task_description");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buildToDo(taskDescription.value);
+    form.reset();
+  });
 });
+
+function buildToDo(todo) {
+  let todoContainer = document.createElement("div");
+  todoContainer.className = "todo_container";
+  let todoItem = document.createElement("p");
+  todoItem.className = "todo_item";
+  todoItem.textContent = todo;
+  let deleteBtn = document.createElement("button");
+  deleteBtn.className = "delete";
+  deleteBtn.textContent = "X";
+  deleteBtn.addEventListener("click", handleDelete);
+  todoContainer.appendChild(todoItem);
+  todoContainer.appendChild(deleteBtn);
+  document.querySelector("#tasks").appendChild(todoContainer);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
